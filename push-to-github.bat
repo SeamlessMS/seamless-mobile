de01071bc0a586e94d5f7a16c5d1e53c4dba7787
SeamlessMS <matt@seamlessms.net>
2025-03-19 15:39:09 -0600

    Initial commit: Seamless Mobile Services

--- push-to-github.bat
+++ push-to-github.bat
@@ -5,4 +5,7 @@
 "C:\Program Files\Git\cmd\git.exe" add vercel.json
 "C:\Program Files\Git\cmd\git.exe" commit -m "Update Vercel configuration for static deployment"
 "C:\Program Files\Git\cmd\git.exe" push
+"C:\Program Files\Git\cmd\git.exe" add public/css/style.css
+"C:\Program Files\Git\cmd\git.exe" commit -m "Optimize popup for mobile devices"
+"C:\Program Files\Git\cmd\git.exe" push
 pause 
